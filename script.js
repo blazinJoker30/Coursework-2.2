517e067cf200e6dbd5aa95129da667b5d752913f
blazinJoker30 <123467060+blazinJoker30@users.noreply.github.com>
2023-04-25 20:28:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-//Used for the menu button when less than 750px
+// Menu button when width less than 750px
 const getElement = (selector) => {
   const element = document.querySelector(selector)
 
   if (element) return element
-  throw Error(`Please double check your class names, there is no ${selector} class`)
+  throw Error(`Please check your class names, there is no ${selector} class`)
 }
-
 const links = getElement('.nav-links')
-const navBtnDOM = getElement('.nav-btn')
+const dropDownBtn = getElement('.nav-btn')
+dropDownBtn.addEventListener('click', () => {links.classList.toggle('show-links')})
 
-navBtnDOM.addEventListener('click', () => {links.classList.toggle('show-links')})
-
-
-// Light/Dark mode
+// Day/Night mode
 var icon = document.getElementById("icon");
-
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
@@ -24,94 +20,81 @@ icon.onclick = function(){
     }
 }
 
-
-
-
-
-//Random drink page selector
+// Random drinks page selector - Smoothie
 function randomLinkSmoothie() {
-  // Define an array of URLs to choose from
+  // Define an array of URLs (to choose from)
   var links = [
     "smoothie-recipe-1.html",
     "smoothie-recipe-2.html"
   ];
 
-  // Generate a random index from 0 to the length of the links array
+  // Generate random index from 0 to array size
   var randomIndex = Math.floor(Math.random() * links.length);
 
-  // Use the random index to select a random link from the array
+  // Use random index to select link from the array
   var randomLink = links[randomIndex];
 
-  // Navigate the user to the selected random link
+  // Navigate user to the selected random link
   window.location.href = randomLink;
 }
 
-
-
-
-
+// Random drinks page selector - Milkshakes
 function randomLinkMilkshakes() {
-    // Define an array of URLs to choose from
-    var links = [
-      "milkshake-recipe-1.html",
-      "milkshake-recipe-2.html"
-    ];
+  // Define an array of URLs (to choose from)
+  var links = [
+    "milkshake-recipe-1.html",
+    "milkshake-recipe-2.html"
+  ];
   
-    // Generate a random index from 0 to the length of the links array
-    var randomIndex = Math.floor(Math.random() * links.length);
+  // Generate random index from 0 to array size
+  var randomIndex = Math.floor(Math.random() * links.length);
   
-    // Use the random index to select a random link from the array
-    var randomLink = links[randomIndex];
+  // Use random index to select link from array
+  var randomLink = links[randomIndex];
   
-    // Navigate the user to the selected random link
-    window.location.href = randomLink;
-  }
+  // Navigate user to the selected random link
+  window.location.href = randomLink;
+}
 
-
-
-  function randomLinkHotDrinks() {
-    // Define an array of URLs to choose from
-    var links = [
-      "hot-drinks-recipe-1.html",
-      "hot-drinks-recipe-2.html"
-    ];
+// Random drinks page selector - HotDrinks 
+function randomLinkHotDrinks() {
+  // Define array of URLs (to choose from)
+  var links = [
+    "hot-drinks-recipe-1.html",
+    "hot-drinks-recipe-2.html"
+  ];
   
-    // Generate a random index from 0 to the length of the links array
-    var randomIndex = Math.floor(Math.random() * links.length);
+  // Generate random index from 0 to array size
+  var randomIndex = Math.floor(Math.random() * links.length);
   
-    // Use the random index to select a random link from the array
-    var randomLink = links[randomIndex];
+  // Use random index to select link from array
+  var randomLink = links[randomIndex];
   
-    // Navigate the user to the selected random link
-    window.location.href = randomLink;
-  }
+  // Navigate user to the selected random link
+  window.location.href = randomLink;
+}
 
-
-
-
-  function randomLinkColdDrinks() {
-    // Define an array of URLs to choose from
-    var links = [
-      "cold-drinks-recipe-1.html",
-      "cold-drinks-recipe-2.html"
-    ];
+// Random drinks page selector - ColdDrinks
+function randomLinkColdDrinks() {
+  // Define array of URLs (to choose from)
+  var links = [
+    "cold-drinks-recipe-1.html",
+    "cold-drinks-recipe-2.html"
+  ];
   
-    // Generate a random index from 0 to the length of the links array
-    var randomIndex = Math.floor(Math.random() * links.length);
+  // Generate random index from 0 to array size
+  var randomIndex = Math.floor(Math.random() * links.length);
   
-    // Use the random index to select a random link from the array
-    var randomLink = links[randomIndex];
+  // Use random index to select link from array
+  var randomLink = links[randomIndex];
   
-    // Navigate the user to the selected random link
-    window.location.href = randomLink;
-  }
-
-
-
-  
+  // Navigate user to the selected random link
+  window.location.href = randomLink;
+}
+ 
+// Carosel (in privacy policy)
 let slideIndex = 0;
 showSlides();
-
 function showSlides() {
   let slides = document.getElementsByClassName("carousel-item");
   for (let i = 0; i < slides.length; i++) {
